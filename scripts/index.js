9cb0b8f1e79d76504e1d659f3ccfcc99e0e04112
Normal_One <nlavrov2007@gmail.com>
2022-08-17 14:47:30 +0200

    final fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 const elementsList = document.querySelector('.elements');
 const elementTemplate = document.querySelector('.element-template').content;
 
-const formElement = document.querySelectorAll('.popup__form');
+const formElementAdd = document.getElementById('add-element');
+const formElementEdit = document.getElementById('edit-profile');
+
+const popupImage = document.querySelector('#popup-image');
+const popupProfile = document.querySelector('#popup-edit');
+const popupCard = document.querySelector('#popup-add');
 
 const buttonClose = document.querySelectorAll('.popup__close-button');
 const buttonEdit = document.querySelector('.profile__edit-button');
@@ -20,7 +25,7 @@ function addElements(data) {
   elementElement.querySelector('.element__title').textContent = data.name;
   elementElement.querySelector('.element__image').src = data.link;
   elementElement.querySelector('.element__image').alt = data.name + ' фото';
-  elementElement.querySelector('.element__image-button').addEventListener('click', handlePreviewPicture)
+  elementElement.querySelector('.element__image-button').addEventListener('click', handlePreviewPicture);
   elementElement.querySelector('.like-button').addEventListener('click', handleLikeIcon);
   elementElement.querySelector('.delete-button').addEventListener('click', handleDeleteIcon);
   return elementElement;
@@ -28,17 +33,18 @@ function addElements(data) {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  if (evt.target.closest('#popup-edit') !== null) {
-    jobValue.textContent = jobInput.value;
-    nameValue.textContent = nameInput.value;
-  } else {
-    let data = [{name: titleInput.value, link: urlInput.value}]
-    data.forEach( element => {
-      elementsList.prepend(addElements(element))
-    });
-    titleInput.value = '';
-    urlInput.value = '';
-  }
+  jobValue.textContent = jobInput.value;
+  nameValue.textContent = nameInput.value;
+  closePopup(evt);
+};
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  let data = [{name: titleInput.value, link: urlInput.value}];
+  data.forEach( element => {
+    elementsList.prepend(addElements(element));
+  });
+  formElementAdd.reset()
   closePopup(evt);
 };
 
@@ -48,41 +54,38 @@ function handleLikeIcon(evt) {
 
 function handleDeleteIcon(evt) {
   evt.target.parentNode.remove();
-}
+};
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 };
 
-function openElementPopup() {
-  openPopup(document.querySelector('#popup-add'))
-}
-
 function handlePreviewPicture(data) {
   document.querySelector('.popup__image-title').textContent = data.target.closest('.element').querySelector('.element__title').textContent;
   document.querySelector('.popup__image').src = data.target.src;
-  openPopup(document.querySelector('#popup-image'))
+  openPopup(popupImage);
 };
 
 function openProfilePopup() {
   nameInput.value = nameValue.textContent;
   jobInput.value = jobValue.textContent;
-  openPopup(document.querySelector('#popup-edit'));
-}
+  openPopup(popupProfile);
+};
 
 function closePopup(popup) {
   popup.target.closest('.popup').classList.remove('popup_opened');
 };
 
-formElement.forEach( element => {
-  element.addEventListener('submit', handleProfileFormSubmit);
-});
+formElementEdit.addEventListener('submit', handleProfileFormSubmit);
+formElementAdd.addEventListener('submit', handleCardFormSubmit);
 buttonClose.forEach( element => {
   element.addEventListener('click', closePopup);
 });
+buttonAdd.addEventListener('click', function () {
+  openPopup(popupCard)
+});
 buttonEdit.addEventListener('click', openProfilePopup);
-buttonAdd.addEventListener('click', openElementPopup);
 
 initialCards.forEach( element => {
-  elementsList.append(addElements(element))
-})
+  elementsList.append(addElements(element));
+});
